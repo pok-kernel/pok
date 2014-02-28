c86634cb60dfdfa45efa2bd626f501d04b27879e
Philipp Eppelt <philipp.eppelt@mailbox.tu-dresden.de>
2014-02-28 16:26:22 +0100

    changes to test the interrupt services for partitions with a native application

--- examples/myHelloWorld/hello.c
+++ examples/myHelloWorld/hello.c
@@ -15,14 +15,56 @@
  */
 
 #include <libc/stdio.h>
+#include <core/time.h>
+#include <core/thread.h>
+#include <core/syscall.h>
+static unsigned counter = 0;
 
+void time_handler( unsigned vector, void* frame )
+{
+  (void) frame;
+  (void)vector;
+  counter++;
+}
+
+
+void register_handler()
+{
+  pok_ret_t ret = pok_syscall2( POK_SYSCALL_IRQ_REGISTER_HANDLER, 0, (uint32_t)&time_handler );
+  if( ret != POK_ERRNO_OK )
+    printf( "Couldn't register handler\n");
+}
+
+static int flag = 0;
 int user_hello( void )
 {
   printf( "Hello POK world!\n");
-  printf( "try sti\n" );
-  asm( "sti\t\n");
-  printf( "tried sti\n");
+  asm volatile ("cli\t\n");
+  printf( "Uhuu interrupts disabled\n");
+  if (flag < 2)
+  {
+    register_handler();
+    flag++;
+  }
+  pok_time_t tmp = 0;
+  uint8_t _level = 0;
+
+  for( ;; )
+  {
+    pok_time_get (&tmp);
+    if( tmp >= 2000 && tmp <= 3000 )
+    {
+      pok_syscall1( POK_SYSCALL_IRQ_PARTITION_DISABLE, _level );
+      printf( "Counter: %u\n", counter );
+    }
+    if( tmp >= 6000 && tmp <= 8000 )
+    {
+      pok_syscall1( POK_SYSCALL_IRQ_PARTITION_ENABLE, _level );
+      printf( "Counter: %u\n", counter );
+    }
+
+    printf( "Clock gettick: %u \n", tmp );
+  }
 
   return 0;
 }
-

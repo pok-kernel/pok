d25d793f2e2ff0a3f9eda8ee623f9acd97687f12
HuaiYuSched <shenyouren@gmail.com>
2014-08-02 08:43:30 +0800

    Add the handler of guest OS

--- examples/partitions-virtual-interrupt/generated-code/cpu/sizes.c
+++
@@ -1,6 +0,0 @@
-#include <types.h>
-uint32_t part_sizes[] = {
-36380
-,
-75504
-};
--- examples/testHypercall/handle.c
+++ examples/testHypercall/handle.c
@@ -1,9 +1,14 @@
 
-
+#define PIT_IRQ 0
+uint64_t pok_tick_counter = 0;
 pok_ret_t handle_irq(uint8_t irq)
 {
-  do{
-  }
+  switch(irq)
+  {
+case PIT_IRQ:
+    pok_tick_counter ++;
+    printf( "Clock gettick: %u \n",pok_tick_counter);
+    
 }
     
 

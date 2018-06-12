35d5b0ff584905c855b6550aaae0b0b1aff1bb0d
borde <etienne.borde@telecom-paristech.fr>
2018-06-12 14:40:48 +0200

    fix bug: semaphore creation needs a locking_policy in lockattr

--- libpok/core/semcreate.c
+++ libpok/core/semcreate.c
@@ -32,6 +32,7 @@ pok_ret_t pok_sem_create (pok_sem_id_t*                        id,
    lockattr.initial_value     = current_value;
    lockattr.max_value         = max_value;
    lockattr.queueing_policy   = discipline;
+   lockattr.locking_policy    = POK_LOCKOBJ_POLICY_STANDARD;
    lockattr.kind              = POK_LOCKOBJ_KIND_SEMAPHORE;
 
    return pok_syscall2 (POK_SYSCALL_LOCKOBJ_CREATE, (uint32_t)id, (uint32_t)&lockattr);

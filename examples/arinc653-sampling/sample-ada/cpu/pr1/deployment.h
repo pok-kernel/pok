a353a7ae160d591344a3b5884d2443c423737302
Samuel Tardieu <sam@rfc1149.net>
2020-06-08 18:14:23 +0200

    Remove wrong copyright header on auto-generated files

--- examples/arinc653-sampling/sample-ada/cpu/pr1/deployment.h
+++ examples/arinc653-sampling/sample-ada/cpu/pr1/deployment.h
@@ -1,17 +1,3 @@
-/*
- *                               POK header
- *
- * The following file is a part of the POK project. Any modification should
- * be made according to the POK licence. You CANNOT use this file or a part
- * of a file for your own project.
- *
- * For more information on the POK licence, please see our LICENCE FILE
- *
- * Please follow the coding guidelines described in doc/CODING_GUIDELINES
- *
- *                                      Copyright (c) 2007-2020 POK team
- */
-
 #ifndef __OCARINA_GENERATED_DEPLOYMENT_H_
 #define __OCARINA_GENERATED_DEPLOYMENT_H_
 /*****************************************************/

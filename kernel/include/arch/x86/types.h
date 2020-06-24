9cd568c5faaf72ad74d4a96b48cd85a599055d4d
Romain Guilloteau <romain.guilloteau@telecom-paris.fr>
2020-06-24 18:43:19 +0200

    Correct uint8_t and int8_t definitions

--- kernel/include/arch/x86/types.h
+++ kernel/include/arch/x86/types.h
@@ -15,12 +15,12 @@
 #ifndef __POK_X86_TYPES_H__
 #define __POK_X86_TYPES_H__
 
-typedef unsigned short uint8_t;
+typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
 
-typedef short int8_t;
+typedef char int8_t;
 typedef short int16_t;
 typedef int int32_t;
 typedef signed long long int64_t;

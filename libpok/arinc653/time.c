7331d0fcd1e3f3dbd9822be527ad0c32fd4982be
borde <etienne.borde@telecom-paris.fr>
2020-12-06 19:13:24 +0100

    backtrack previous commit; need to check if pok_time_get should return micro or nanoseconds

--- libpok/arinc653/time.c
+++ libpok/arinc653/time.c
@@ -35,7 +35,6 @@ void PERIODIC_WAIT(RETURN_CODE_TYPE *return_code) {
 
 void GET_TIME(SYSTEM_TIME_TYPE *system_time, RETURN_CODE_TYPE *return_code) {
   *return_code = pok_time_get((pok_time_t *)system_time);
-  *system_time = *system_time * 1000;
 }
 
 #ifndef POK_CONFIG_OPTIMIZE_FOR_GENERATED_CODE
